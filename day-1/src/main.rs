4d2f8f961684c32ccd71a4600325d2e08b48b130
Jack Hales <halesyy@gmail.com>
2024-02-27 13:07:07 +1100

    More concise d1p2 piece.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -22,28 +22,6 @@ use aho_corasick::AhoCorasick;
 
 const INPUT_FILE: &str = "input.txt";
 
-// fn consume_parse_into_line_value(line: &str) -> u32 {
-//     let digits: Vec<u32> = line
-//         .chars()
-//         .filter_map(|c| {
-//             if c.is_numeric() {
-//                 // Parse the char -> string -> parse into u32 from str.
-//                 let parsed_u32: u32 = String::from(c).parse()
-//                     .expect(format!("Could not parse {} into a string/u32", c).as_str());
-//                 Some(parsed_u32)
-//             } else {
-//                 None
-//             }
-//         })
-//         .collect();
-//     // Calculate the values based on first/last of the array.
-//     let a = digits.first().unwrap();
-//     let b = digits.last().unwrap();
-//     let interp = format!("{}{}", a, b);
-//     let interp_num: u32 = interp.parse().unwrap();
-//     interp_num
-// }
-
 const DIGIT_PATTERNS: [&str; 18] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     "1", "2", "3", "4", "5", "6", "7", "8", "9"
@@ -63,6 +41,7 @@ fn line_to_combined_digit(line: &str) -> u32 {
         // 0 = "one", 1 = "two", etc. so last digit char is for 
         // specifically 8, 9+ are direct matches.
         let pattern_id = digit_word_match.pattern().as_usize();
+        
         // Get the string.
         let digit_string = match pattern_id {
             // 1 .. 8
@@ -77,13 +56,16 @@ fn line_to_combined_digit(line: &str) -> u32 {
         string_digits.push(digit_string);
     }
 
+    // Print out the line and its resulting outcome.
     println!("{} -> {:?}", line, string_digits);
 
+    // Crete the A-part, B-part, combine them, and parse it into the output.
     let a = string_digits.first().unwrap();
     let b = string_digits.last().unwrap();
     let combined_ab = format!("{}{}", a, b);
 
-    combined_ab.parse::<u32>().unwrap()
+    let output = combined_ab.parse::<u32>().unwrap();
+    output
 }
 
 fn main() {
@@ -96,15 +78,6 @@ fn main() {
 
     for line in lines.into_iter() {
         let val = line_to_combined_digit(line);
-
-        // continue;
-
-        // println!("{line}\n->\n{val}\n");
-        // let fl_value = consume_parse_into_line_value(&repline);
-
-        // let replaced_line = string_digit_replace(line);
-        // let val = consume_parse_into_line_value(&replaced_line);
-        // println!("{} = {} = {}", line, replaced_line, val);
         running_sum += val;
     }
 
